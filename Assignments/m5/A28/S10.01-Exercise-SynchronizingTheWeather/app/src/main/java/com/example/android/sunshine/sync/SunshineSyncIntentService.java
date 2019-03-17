03ba1d6aaa1d6adcadedcf0991a76c6be8f431ca
priyankamaladi77 <svanmp@msitprogram.net>
2019-03-17 23:49:14 +0530

    A28

---
+++ Assignments/m5/A28/S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -0,0 +1,24 @@
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ */
+public class SunshineSyncIntentService extends IntentService {
+
+    //  TODO (6) Create a constructor that calls super and passes the name of this class
+    public SunshineSyncIntentService() {
+        super("SunshineSyncIntentService");
+    }
+
+    //  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        SunshineSyncTask.syncWeather(this);
+    }
+}
